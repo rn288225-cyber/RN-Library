b8767dca57d4cd3a7e108b785ee4ed5e9fdbd86f
Rohit Nayak <rn288225@gmail.com>
2026-09-23 14:41:59 +0530

    Build RN Library homepage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("RN Library JavaScript is working!");
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", function () {
+
+    const query = searchInput.value.trim();
+
+    if (query === "") {
+        alert("Please enter a book name.");
+        return;
+    }
+
+    alert("Searching for: " + query);
+});
